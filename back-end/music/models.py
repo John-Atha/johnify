b517e7e79f2b13d32dae24d6a283202ffd1fa88d
John-Atha <giannisj3@gmail.com>
2021-05-14 11:46:00 +0300

    Add photo field to users, albums & songs

--- back-end/music/models.py
+++ back-end/music/models.py
@@ -8,10 +8,12 @@ from django.db.models.fields.related import ManyToManyField
 
 class User(AbstractUser):
     is_artist = BooleanField(default=False)
+    photo = models.ImageField(default='', null=True)
     def __str__(self):
         return f"{self.id}, {self.username}, {self.is_artist}"
 
 class Album(models.Model):
+    photo = models.ImageField(default='', null=True)
     artist = models.ForeignKey(User, on_delete=models.CASCADE, related_name="albums", null=False)
     title = CharField(max_length=100, null=False)
     date = models.DateField(default=date.today)
@@ -20,9 +22,10 @@ class Album(models.Model):
         return f"{self.id}, {self.title}"
 
 class Song(models.Model):
+    photo = models.ImageField(default='', null=True)
     title = CharField(max_length=100, null=False)
     file = FileField(null = False)
-    album = models.ForeignKey(Album, on_delete=models.CASCADE, related_name="songs", null=False)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE, related_name="songs", null=False, blank=False)
     date = models.DateField(default=date.today)
     fans = models.ManyToManyField(User, related_name='fav_songs')
     def __str__(self):
