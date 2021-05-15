dcbba25346e35a18818c7bd92f2c93b0d7afc5eb
John-Atha <giannisj3@gmail.com>
2021-05-15 14:08:37 +0300

    Update register endpoint and add post-album endpoint

--- back-end/music/urls.py
+++ back-end/music/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('login', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('refresh', TokenRefreshView.as_view(), name='token_refresh'),
     path('users', views.Users.as_view(), name='users'),
-    path('users/<int:id>', views.OneUser.as_view(), name='oneUser'),
+    path('users/<int:id>', views.OneUser.as_view(), name='one-user'),
+    path('albums', views.Albums.as_view(), name='albums'),
+    path('albums/<int:id>', views.OneAlbum.as_view(), name='one-album')
 ]
--- back-end/music/views.py
+++ back-end/music/views.py
@@ -1,5 +1,5 @@
 import json
-from music.serializers import UserSerializer
+from music.serializers import UserSerializer, AlbumSerializer, UserCreateSerializer
 from music.models import *
 from django.shortcuts import render
 from rest_framework.views import APIView
@@ -36,27 +36,44 @@ def paginate(start, end, items):
 
 class Users(APIView):
     permission_classes = [permissions.AllowAny]
+    
     def get(self, request):
-        users = User.objects.all()
-        users = paginate(request.GET.get('start'), request.GET.get('end'), users)
+        users = paginate(request.GET.get('start'), request.GET.get('end'), User.objects.all())
+        # pagination successfull
         try:
             users = [UserSerializer(user).data for user in users]
+        # exception here -> pagination had returned an error
         except Exception:
             return users
         return Response(users, status=status.HTTP_200_OK)
     
     @transaction.atomic
     def post(self, request):
-        body = json.loads(request.body)
-        user = UserSerializer(data=body)
-        if user.is_valid():
-            user.save()
-            return Response(user.data, status=status.HTTP_200_OK)
+        if 'username' in request.POST and 'password' in request.POST and 'confirmation' in request.POST and 'email' in request.POST:
+            username = request.POST['username']
+            password = request.POST['password']
+            confirmation = request.POST['confirmation']
+            email = request.POST['email']
+            if 'is_artist' in request.POST:
+                is_artist = request.POST['is_artist']==1
+            else:
+                is_artist = False
+            if password==confirmation:
+                user = UserCreateSerializer(data=request.POST)
+                if user.is_valid():
+                    user.save()
+                    return Response(user.data, status.HTTP_200_OK)
+                else:
+                    return Response(user.errors, status=status.HTTP_400_BAD_REQUEST)
+            else:
+                return Response("Passwords don't match", status=status.HTTP_400_BAD_REQUEST)
         else:
-            return Response(user.errors, status=status.HTTP_400_BAD_REQUEST) 
+            return Response("Compulsory fields missing.", status=status.HTTP_400_BAD_REQUEST)
+
 
 class OneUser(APIView):
     permission_classes = [permissions.AllowAny]
+    
     def get(self, request, id):
         try:
             user = User.objects.get(id=id)
@@ -86,7 +103,7 @@ class OneUser(APIView):
         try:
             user = User.objects.get(id=id)
         except User.DoesNotExist:
-            return Response(f"User '{id}' does not exist.", status=status.HTTP_400_BAD_REQUEST)
+            return Response(f"User '{id}' not found.", status=status.HTTP_404_NOT_FOUND)
         if request.user==user:
             operation = user.delete()
             if operation:
@@ -95,3 +112,56 @@ class OneUser(APIView):
                 return Response('Deletion failed', status=status.HTTP_400_BAD_REQUEST)
         else:
             return Response('Unauthorized', status=status.HTTP_401_UNAUTHORIZED)
+
+class Albums(APIView):
+    permission_classes = [permissions.AllowAny]
+    
+    def get(self, request):
+        albums = paginate(request.GET.get('start'), request.GET.get('end'), Album.objects.all())
+        # pagination successfull
+        try:
+            albums = [AlbumSerializer(album).data for album in albums]
+        # exception here -> pagination had returned an error
+        except Exception:
+            return albums
+        return Response(albums, status=status.HTTP_200_OK)
+    
+    # I expect title and artist id
+    @transaction.atomic
+    def post(self, request):
+        if request.user.is_anonymous:
+            return Response("Unauthorized", status=status.HTTP_401_UNAUTHORIZED)
+        else:
+            user = request.user
+            if not user.is_artist:
+                return Response("Only artists can create albums.", status=status.HTTP_400_BAD_REQUEST)
+            else:
+                if 'title' in request.POST:
+                    title = request.POST['title']
+                    try:
+                        album = user.albums.get(title=title)
+                        return Response('You cannot have two albums with the same title.', status=status.HTTP_400_BAD_REQUEST)
+                    except Album.DoesNotExist:
+                        album = Album(artist=user, title=title)
+                        album.save()
+                        return Response(AlbumSerializer(album).data, status=status.HTTP_200_OK)
+                else:
+                    return Response('Title missing', status=status.HTTP_400_BAD_REQUEST)
+
+class OneAlbum(APIView):
+    permission_classes = [permissions.AllowAny]
+    
+    def get(self, request, id):
+        try:
+            album = Album.objects.get(id=id)
+        except Album.DoesNotExist:
+            return Response(f"Album '{id}' not found.", status=status.HTTP_404_NOT_FOUND)
+        return Response(AlbumSerializer(album).data, status=status.HTTP_200_OK)
+    
+    @transaction.atomic
+    def put(self, request, id):
+        pass
+    
+    @transaction.atomic
+    def delete(self, request, id):
+        pass
