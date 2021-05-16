05e0d1514cbc1a6ef88dca88e4ae7401be030aa0
John-Atha <giannisj3@gmail.com>
2021-05-16 18:12:11 +0300

    Add front-end skeleton and allow cors on backend

--- back-end/johnify/settings.py
+++ back-end/johnify/settings.py
@@ -29,6 +29,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://192.168.2.8:3000"
+] 
+
+CSRF_COOKIE_DOMAIN = "http://localhost:3000/"
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=20),
